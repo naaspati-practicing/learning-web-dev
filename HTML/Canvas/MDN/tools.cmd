35e0395ba0a9eeba38e7abcc2fa169a910c73b62
naaspati <naaspati@gmail.com>
2019-06-20 09:42:10 +0530

    minor

---
+++ HTML/Canvas/MDN/tools.cmd
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+if [%1]==[] (
+    echo no command specified
+    goto:eof
+)
+
+if %1==s goto:server
+if %1==server goto:server
+
+
+
+
+goto:eof
+
+:server
+if [%2]==[] (
+    echo no root dir specified
+    echo usage: tools s ROOT_DIR
+    goto:eof
+)
+
+if not exist %2 (
+    echo dir not found: %2
+    goto:eof
+)
+
+cd %2
+start cmd /c live-server
+
+goto:eof
